0fbcfb6969dc885db31d14ba3a810b8aa1c3c01f
Saint <max161125@gmail.com>
2025-11-11 13:03:27 +0300

    add recursion object compare

--- src/parsers.js
+++ src/parsers.js
@@ -4,23 +4,53 @@ import _ from 'lodash';
 import yaml from 'js-yaml';
 
 const getFullPath = (filePath) => path.resolve(filePath);
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return String(value)
+  }
 
-const parseData = (data1, data2) => {
-  const sortedKeys = _.union(Object.keys(data1), Object.keys(data2)).sort();
-  const diff = sortedKeys.reduce((acc, key) => {
-    if (Object.hasOwn(data1, key)) {
-      if (data1[key] === data2[key]) acc.push(`    ${key}: ${data1[key]}`);
-      else {
-        acc.push(`  - ${key}: ${data1[key]}`);
-        if (Object.hasOwn(data2, key)) acc.push(`  + ${key}: ${data2[key]}`);
-      }
-    } else acc.push(`  + ${key}: ${data2[key]}`);
+  const indent = ' '.repeat(depth * 4);
+  const bracketIndent = ' '.repeat((depth - 1) * 4);
+  const entries = Object.entries(value)
+    .map(([key, val]) => `${indent}${key}: ${stringify(val, depth + 1)}`)
+    .join('\n');
 
-    return acc;
-  }, ['{']);
+  return `{\n${entries}\n${bracketIndent}}`;
+};
 
-  diff.push('}');
-  return diff.join('\n');
+const parseData = (data1, data2, depth = 1) => {
+  const indent = ' '.repeat(depth * 4 - 2);
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+
+  const lines = keys.map((key) => {
+    const val1 = data1[key];
+    const val2 = data2[key];
+
+    if (_.isObject(val1) && _.isObject(val2)) {
+      const nested = parseData(val1, val2, depth + 1);
+      return `${' '.repeat(depth * 4)}${key}: ${nested}`;
+    }
+
+    if (!Object.hasOwn(data2, key)) {
+      return `${indent}- ${key}: ${stringify(val1, depth + 1)}`;
+    }
+
+    if (!Object.hasOwn(data1, key)) {
+      return `${indent}+ ${key}: ${stringify(val2, depth + 1)}`;
+    }
+
+    if (_.isEqual(val1, val2)) {
+      return `${' '.repeat(depth * 4)}${key}: ${stringify(val1, depth + 1)}`;
+    }
+
+    return [
+      `${indent}- ${key}: ${stringify(val1, depth + 1)}`,
+      `${indent}+ ${key}: ${stringify(val2, depth + 1)}`
+    ].join('\n');
+  });
+
+  const bracketIndent = ' '.repeat((depth - 1) * 4);
+  return `{\n${lines.join('\n')}\n${bracketIndent}}`;
 };
 
 const parseFiles = (filePath1, filePath2) => {
