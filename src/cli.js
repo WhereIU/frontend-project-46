b431821deea5c4010fcefe6e9f787ffdd85e48c5
Saint <max161125@gmail.com>
2025-11-12 12:32:41 +0300

    fix waste artifacts in code, linter issues

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filePath1, filePath2, { format }) => console.log(formatDiff(filePath1, filePath2, format))
+  .action(
+    (filePath1, filePath2, { format }) => console.log(formatDiff(filePath1, filePath2, format)),
   );
 
 program.parse();
